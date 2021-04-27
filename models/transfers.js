f0cf61852d83032cd4ba0adb4996a945c658b5d8
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-04-27 20:59:10 +0200

    feat(models) added values in transfershema

--- models/transfers.js
+++ models/transfers.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const Shema = mongoose.Schema;
 const transferShema = new Shema({
-    
+    userFrom: String,
+    userTo : String,
+    coins: Number,
+    discription: String
 })
 
 const Transfer = mongoose.model('Transfer', transferShema);
