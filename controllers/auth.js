51d567c4183d7f286b6c44d546532c82b586e032
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-04-28 19:54:14 +0200

    fix(controller) set username for signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,16 @@
 const User = require('../models/user');
 
 const signUp = async (req, res, next) => {
-    let firstName = req.body.firstName;
-    let lastName = req.body.lastName;
+    // let firstname = req.body.firstname;
+    // let lastName = req.body.lastName;
+    let username = req.body.username;
     let password = req.body.password;
     let coins = req.body.coins;
 
     const user = new User({
-        firstName: firstName,
-        lastName: lastName,
+        // firstName: firstName,
+        // lastName: lastName,
+        username: username,
         coins: coins
     });
     await user.setPassword(password);
