4bbdb1b32aa18ca0de770a9497fef7cfc686dde1
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-05-15 16:01:47 +0200

    feat(leaderboard) added leaderboard

--- models/leaderboard.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const leaderboardSchema = new Schema({
-})
-
-const Leaderboard = mongoose.model('Leaderboard', leaderboardSchema);
-
-module.exports = Leaderboard;
