de84cd520d1657c605d4a7ff3e04a1978c9f0270
stephanielambrighs <stephanielambrighs@hotmail.com>
2021-05-20 10:25:05 +0200

    feat(cssMinify) added css minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,36 @@
-const { src, dest, watch } = require('gulp');
+const {src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
 sass.compiler = require('node-sass');
-
 const minify = require('gulp-minify');
+let cssmin = require('gulp-cssmin');
+let rename = require('gulp-rename');
 
 
-sass2css = function () {
+exports.sass2css = function () {
   return src('./sass/app.scss')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(dest('./public/stylesheets/dist/'))
-} 
+    .pipe(sass().on('error', sass.logError))
+    .pipe(dest('./public/stylesheets/dist/'))
+}
 
-  jsMinify = function () {
-    return src(['./public/javascripts/*.js'])
+
+exports.jsMinify = function () {
+  return src(['./public/javascripts/*.js'])
     .pipe(minify({
     }))
     .pipe(dest('./public/javascripts/dist/app.js'))
 }
 
 
-exports.default = function () {
-    watch('./sass/**/*.scss', sass2css) 
-    watch('./public/javascripts/*.js', jsMinify)
+exports.cssMinify = function (){
+  return src(['./public/stylesheets/dist/app.css'])
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(dest('./public/dist/'));
+}
 
-   
-  }
+
+// exports.default = function () {
+//   watch('./sass/**/*.scss', sass2css)
+//   watch('./public/javascripts/*.js', jsMinify)
+//   watch('./public/stylesheets/dist/app.css', cssMinify)
+// }
